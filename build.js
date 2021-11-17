753135cb2521bbb6a3e0ca32d8f4d1be8a8fca32
Mohammed Zughayer <mohammed.zughayer@rubikomm.com>
2021-11-17 14:02:43 +0200

    new version v1.1.12.1 stagging

--- build.js
+++ build.js
@@ -5,21 +5,21 @@ packager({
     buildPath: __dirname,
     electronVersion: '10.1.3',
     version: '1.1.0',
-    buildVersion: '1.1.11',
+    buildVersion: '1.1.12.1',
     dir: __dirname,
     overwrite: true,
     asar: true,
     platform: 'win32',
     arch: 'ia32',
-    icon: 'assets/icons/maestro.ico',
+    icon: 'assets/icons/app.ico',
     prune: true,
     appVersion: "1.1.11",
     win32metadata: {
         CompanyName: "Rubikomm",
-        ProductName: "MaestroPOS",
+        ProductName: "CloudPOS",
     },
     afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
-        rebuild.rebuild({buildPath, electronVersion, arch})
+        rebuild.rebuild({ buildPath, electronVersion, arch })
             .then(() => callback())
             .catch((error) => callback(error));
     }],
