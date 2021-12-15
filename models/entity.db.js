7e77cb37527441825c3d269fc5e32552668ce79e
Mohammed Zughayer <mohammed.zughayer@rubikomm.com>
2021-12-15 12:09:55 +0200

    fix some db model relation issues

--- models/entity.db.js
+++ models/entity.db.js
@@ -15,11 +15,9 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
     name_ar: { type: DataTypes.STRING, defualtValue: null, allowNull: true },
     name_en: { type: DataTypes.STRING, defualtValue: null, allowNull: true },
-    allow_discount: { type: DataTypes.TINYINT, defualtValue: 0, allowNull: true },
-    discount_type: { type: DataTypes.ENUM('percentage', 'value', 'undefined'), allowNull: false },
+    discount_type: { type: DataTypes.ENUM('percentage', 'value', 'undefined'), allowNull: true },
     discount_value: { type: DataTypes.DOUBLE, defualtValue: null, allowNull: true },
-    allow_tax_exemption: { type: DataTypes.BIGINT, defualtValue: null, allowNull: true },
-    tax_exemption_number: { type: DataTypes.STRING, defualtValue: null, allowNull: true }
+    tax_exemption_value: { type: DataTypes.STRING, defualtValue: null, allowNull: true }
   }, {
     sequelize,
     underscored: true,
--- models/variant_categories.db.js
+++ models/variant_categories.db.js
@@ -3,22 +3,22 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class VariantCategory extends Model {
         static associate(models) {
-            VariantCategory.belongsTo(models.variant, { foreignKey: 'item_id', targetKey: 'id' });
-            VariantCategory.belongsTo(models.category, { foreignKey: 'category_id', targetKey: 'id' });
+            VariantCategory.belongsTo(models.variant, { foreignKey: 'item_id', targetKey: 'id', constraints: false });
+            VariantCategory.belongsTo(models.category, { foreignKey: 'category_id', targetKey: 'id', constraints: false });
         }
     }
 
     VariantCategory.init({
-        // id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
         item_id: {
-            type: DataTypes.BIGINT, allowNull: false, primaryKey: false,
+            type: DataTypes.BIGINT, allowNull: true, primaryKey: false,
             references: {
                 model: 'variant',
                 key: 'id'
             }
         },
         category_id: {
-            type: DataTypes.BIGINT, allowNull: false, references: {
+            type: DataTypes.BIGINT, allowNull: true, primaryKey: false,
+            references: {
                 model: 'category',
                 key: 'id'
             }
