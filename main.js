9f3f0db8685b89ebb873149f2f415f421d406653
Mohammed Zughayer <mohammed.zughayer@rubikomm.com>
2021-12-16 13:17:48 +0200

    - fix init tray issue. - add new folder for logos. - delete unused files (XPrinter file).

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ const windowManager = require('./services/window-manager-service');
 const notificationService = require('./services/notification-service');
 const { app, BrowserWindow, Tray } = require('electron');
 const { APPNAME } = require('./commons');
+const path = require('path'); // import path module
 
-let tray = null;
+
+let tray;
 app.disableHardwareAcceleration();
 app.whenReady().then(() => require('./services/index'));
 
-app.on('ready', async () => {
+app.on('ready', async _ => {
    app.setAppUserModelId(APPNAME);
    windowManager.showSplash();
    notificationService.showNotification('App Initiating', 'app is loading important data ...');
    await db.setup().then(() => {
-      windowManager.createAppWindow();
+      tray = new Tray(path.join(__dirname, 'assets', 'logo', 'iisal_logo.png'));
       windowManager.initTray(tray);
+      windowManager.createAppWindow();
       notificationService.showNotification('App is ready', 'app has successfully initiated');
    }).catch((error) => {
       notificationService.showNotification('Error', 'Something went wrong initiating,please contact the support team. ');
