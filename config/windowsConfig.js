e21b8413113883266c440bb93b16bafaa46b88ed
Mohammed Zughayer <mohammed.zughayer@rubikomm.com>
2021-12-16 10:55:30 +0200

    disable devtools for maestro pos version v1.1.13

--- config/windowsConfig.js
+++ config/windowsConfig.js
@@ -22,7 +22,7 @@ module.exports = {
             nodeIntegration: true,
             safeDialogs: false,
             javascript: true,
-            devTools: true,
+            devTools: false,
             webgl: true,
             webSecurity: false,
             nodeIntegrationInWorker: true,
