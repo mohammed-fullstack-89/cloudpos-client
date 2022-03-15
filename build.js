b81bbad7de71f878e819278bda4420f3b14c9730
Mohammed Zughayer <mohammed.zughayer@rubikomm.com>
2022-03-15 18:22:43 +0300

    v1.1.15 for live

--- build.js
+++ build.js
@@ -11,12 +11,12 @@ packager({
     asar: true,
     platform: 'win32',
     arch: 'ia32',
-    icon: 'assets/icons/iisal.ico',
+    icon: 'assets/icons/cloudpos.ico',
     prune: true,
     appVersion: "1.1.15",
     win32metadata: {
         CompanyName: "Rubikomm",
-        ProductName: "CloudPOS(iisal)"
+        ProductName: "CloudPOS"
     },
     afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
         rebuild.rebuild({ buildPath, electronVersion, arch })
--- services/window-manager-service.js
+++ services/window-manager-service.js
@@ -52,7 +52,7 @@ class InitializerService {
         ];
 
         const contextMenu = Menu.buildFromTemplate(trayMenu);
-        tray.setToolTip('CloudPOS(iisal)');
+        tray.setToolTip('CloudPOS');
         tray.setContextMenu(contextMenu);
     }
 
@@ -186,7 +186,7 @@ class InitializerService {
         this.mainWindow.webContents.on("before-input-event", async (event, input) => {
             utility.barcode(event, input);
         });
-        this.mainWindow.loadURL(enviroment.IisalPOS_SA.url);
+        this.mainWindow.loadURL(enviroment.stagging.url);
         this.hideSplash();
     }
 
